e889ef0bef04e43c8a956b287e6e8a9d83da9cf5
Dariell Vasquez <dariellv@yahoo.com>
2018-12-13 13:48:55 -0500

    Modal is now reusable component, added styling

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 
 
 const Modal = props =>{
+    const {title, content, actions, onDismiss} = props;
     return ReactDOM.createPortal(
-        <div className='ui dimmer modals visible active'>
-            <div className='ui standard visible modal active'>
-                My modal
+        <div 
+        onClick={onDismiss}
+        className='ui dimmer modals visible active'>
+            <div 
+            onClick={(e)=>{e.stopPropagation()}} 
+            className='ui standard visible modal active'>
+                <div className='header'>{title}</div>
+                <div className='content'>{content}</div>
+                <div className='actions'>{actions}</div>
             </div>
         </div>,
         document.getElementById('modal')
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Modal from '../Modal';
+import history from '../../history';
 
 const StreamDelete = () =>{
+    const actions = (
+        <React.Fragment>
+            <button className='ui button negative'>Delete</button>
+            <button className='ui button'>Cancel</button>
+        </React.Fragment>
+    )
     return (
         <div>
             Stream Delete
-            <Modal />
+            <Modal 
+            title='Delete Stream'
+            content='Are you sure you want to delete this stream?'
+            onDismiss={()=>history.push('/')}
+            actions={actions}
+            />
         </div>
     )
 }
