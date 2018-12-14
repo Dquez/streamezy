c1923899ca020a62dab8241cfbf25a1ea6f426bb
Dariell Vasquez <dariellv@yahoo.com>
2018-12-14 16:36:17 -0500

    Streaming with OBS transmitted through rtmp server working

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -18,6 +18,10 @@ class StreamShow extends Component{
         this.buildPlayer();
     }
 
+    componentWillUnmount(){
+        this.player.destroy();
+    }
+
     buildPlayer = () =>{
         if(this.player || !this.props.stream){
             return;
