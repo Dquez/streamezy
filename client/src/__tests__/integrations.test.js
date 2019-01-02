dfe35dd5b3d081923b025fec1ceea119ffe7ca5c
Dariell Vasquez <dariellv@yahoo.com>
2019-01-02 17:19:49 -0500

    Display form test and input change for form test working

--- client/src/__tests__/integrations.test.js
+++ client/src/__tests__/integrations.test.js
@@ -72,32 +72,38 @@ describe('StreamShow', ()=>{
 })
 
 describe('StreamEdit', ()=>{
-    it("can display a form to edit a specific stream when you click on the edit button", (done)=>{
+    it("can display a form to edit a specific stream when you click on the edit button", ()=>{
         wrapper.find('.home').at(0).simulate('click',  { button: 0 });
         wrapper.find('.edit-stream').at(0).simulate('click',  { button: 0 });
-        expect(wrapper.containsMatchingElement(<StreamForm/>)).toBeTruthy();
-        let editedStream = streams['5c245be47ce56d21479e6eca'];
-        editedStream.description = 'edited Stream';
-        moxios.install();
-        moxios.stubRequest('/api/streams/5c245be47ce56d21479e6eca', {
-            status: 200,
-            response: editedStream
-        })
+        expect(wrapper.containsMatchingElement(<StreamForm/>)).toBeTruthy();   
+    })
+    it('can update the form inputs when a user types in the respective Field components', ()=>{
+
         wrapper.find('input#title').simulate('change', {
             target: { value: 'Stream test' }
         })
         wrapper.find('input#description').simulate('change', {
             target: { value: 'edited Stream' }
         })
-        // console.log(wrapper.debug());
-        wrapper.find('.form').simulate('submit');
-        moxios.wait(()=> {
-            // wrapper.find('.home').at(0).simulate('click',  { button: 0 });
-            wrapper.update();
-            console.log(wrapper.debug());
-            expect(wrapper.find('.description').text()).toEqual('edited Stream');
-            moxios.uninstall();
-            done();
-        })    
+        console.log(wrapper.debug());
+        expect(wrapper.find('input#title').props().value).toEqual('Stream test')
+        expect(wrapper.find('input#description').props().value).toEqual('edited Stream')
+
     })
+    // it('can update the stream in the redux store/DB and display the updated stream once redirected', (done) =>{
+    //     let editedStream = {...streams['5c245be47ce56d21479e6eca']};
+    //     editedStream.description = 'edited Stream';
+    //     moxios.install();
+    //     moxios.stubRequest('/api/streams/5c245be47ce56d21479e6eca', {
+    //         status: 200,
+    //         response: editedStream
+    //     })
+    //     wrapper.find('.form').simulate('submit');
+    //     moxios.wait(()=> {
+    //         wrapper.update();
+    //         expect(wrapper.find('.description').text()).toEqual('edited Stream');
+    //         moxios.uninstall();
+    //         done();
+    //     }) 
+    // })
 })
