6de62a4aa127734dd023900a498883f57c58f61a
Dariell Vasquez <dariellv@yahoo.com>
2019-01-16 14:02:46 -0500

    refactored streamShow compoenent to reliable produce true for whether video exists

--- client/src/__tests__/integrations.test.js
+++ client/src/__tests__/integrations.test.js
@@ -74,59 +74,62 @@ describe('StreamCreate', ()=>{
     })
 })
 
-// describe('StreamShow', ()=>{
-//     it("can display fetch a specific streams when you click on the stream's link tag", (done)=>{
-//         moxios.install();
-//         moxios.stubRequest('/api/streams/5c245be47ce56d21479e6eca', {
-//             status: 200,
-//             response: streams['5c245be47ce56d21479e6eca']
-//         })
-//         wrapper.find('.home').at(0).simulate('click',  { button: 0 });
-//         wrapper.find('.header').at(0).simulate('click',  { button: 0 });
-//         moxios.wait(()=> {
-//             wrapper.update();
-//             expect(wrapper.containsMatchingElement(<video/>)).toBeTruthy();
-//             moxios.uninstall();
-//             done();
-//         })    
-//     })
-// })
+describe('StreamShow', ()=>{
+    it("can display fetch a specific stream when you click on the stream's link tag", (done)=>{
+        moxios.install();
+        moxios.stubRequest('/api/streams/5c245be47ce56d21479e6eca', {
+            status: 200,
+            response: streams['5c245be47ce56d21479e6eca']
+        })
+        wrapper.find('.home').at(0).simulate('click',  { button: 0 });
+        wrapper.find('.header').at(0).simulate('click',  { button: 0 });
+        moxios.wait(()=> {
+            wrapper.update();
+            expect(wrapper.find('.video').exists()).toEqual(true);
+            moxios.uninstall();
+            done();
+        })    
+    })
+})
 
-// describe('StreamEdit', ()=>{
-//     it("can display a form to edit a specific stream when you click on the edit button", ()=>{
-//         wrapper.find('.home').at(0).simulate('click',  { button: 0 });
-//         wrapper.find('.edit-stream').at(0).simulate('click',  { button: 0 });
-//         expect(wrapper.containsMatchingElement(<StreamForm/>)).toBeTruthy();   
-//     })
-//     it('can update the form inputs and stream in the redux store/DB and display the updated stream once redirected', (done) =>{
-//         wrapper.find('input#title').simulate('change', {
-//             target: { value: 'Test' }
-//         })
-//         wrapper.find('input#description').simulate('change', {
-//             target: { value: 'edited' }
-//         })
-//         expect(wrapper.find('input#title').props().value).toEqual('Test')
-//         expect(wrapper.find('input#description').props().value).toEqual('edited')
-//         // mocking the patched stream that would be sent back from the server
-//         let editedStream = {...streams['5c245be47ce56d21479e6eca']};
-//         editedStream.title = 'Test';
-//         editedStream.description = 'edited';
+describe('StreamEdit', ()=>{
+    it("can display a form to edit a specific stream when you click on the edit button", ()=>{
+        wrapper.find('.home').at(0).simulate('click',  { button: 0 });
+        wrapper.find('.edit-stream').at(0).simulate('click',  { button: 0 });
+        expect(wrapper.containsMatchingElement(<StreamForm/>)).toBeTruthy();   
+    })
+    // it('can update the form inputs and stream in the redux store/DB and display the updated stream once redirected', (done) =>{
+    //     wrapper.find('input#title').simulate('change', {
+    //         target: { value: 'Test' }
+    //     })
+    //     wrapper.find('input#description').simulate('change', {
+    //         target: { value: 'edited' }
+    //     })
+ 
+    //     expect(wrapper.find('input#title').props().value).toEqual('Test')
+    //     expect(wrapper.find('input#description').props().value).toEqual('edited')
+    //     // mocking the patched stream that would be sent back from the server
+    //     let editedStream = {...streams['5c245be47ce56d21479e6eca']};
+    //     editedStream.title = 'Test';
+    //     editedStream.description = 'edited';
+    //     // console.log(editedStream);
         
-//         moxios.install();
-//         moxios.stubRequest('/api/streams/5c245be47ce56d21479e6eca', {
-//             status: 200,
-//             response: editedStream
-//         })
-//         wrapper.find('.form').simulate('submit');
-//         moxios.wait(()=> {
-//             wrapper.update();
-//             expect(wrapper.find('.header').at(1).text()).toEqual('Test');
-//             expect(wrapper.find('.description').text()).toEqual('edited');
-//             moxios.uninstall();
-//             done();
-//         }) 
-//     })
-// })
+    //     moxios.install();
+    //     moxios.stubRequest('/api/streams/5c245be47ce56d21479e6eca', {
+    //         status: 200,
+    //         response: editedStream
+    //     })
+    //     wrapper.find('.form').simulate('submit');
+    //     moxios.wait(()=> {
+    //         wrapper.update();
+    //         console.log(wrapper.debug());
+    //         expect(wrapper.find('.header').at(1).text()).toEqual('Test');
+    //         expect(wrapper.find('.description').text()).toEqual('edited');
+    //         moxios.uninstall();
+    //         done();
+    //     }) 
+    // })
+})
 
 // describe('StreamDelete', ()=>{
 //     it("can display a modal to delete a specific stream when you click on the delete button from the homepage", ()=>{
