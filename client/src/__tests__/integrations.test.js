288dcbf8c5a9fc87d0a9ff57e00ae21bb8949f5f
Dariell Vasquez <dariellv@yahoo.com>
2019-01-18 14:50:42 -0500

    StreamEdit working, can edit an existing document in DB

--- client/src/__tests__/integrations.test.js
+++ client/src/__tests__/integrations.test.js
@@ -6,7 +6,7 @@ import StreamForm from '../components/streams/StreamForm';
 import Modal from '../components/Modal';
 import _ from 'lodash';
 import moxios from 'moxios';
-import { debug } from 'util';
+import axios from 'axios';
 
 let wrapper;
 let streams;
@@ -14,10 +14,10 @@ let auth;
 let initialState;
 beforeEach(()=>{
     streams = {
-        '5c245be47ce56d21479e6eca': {
-            _id: '5c245be47ce56d21479e6eca',
-            title: 'Stream test',
-            description: 'streams',
+        '5c422bad69726ddc44b48d4b': {
+            _id: '5c422bad69726ddc44b48d4b',
+            title: 'My first Stream',
+            description: 'Streamezy',
             userId: '104172360244756468300'
         }  
     }
@@ -69,44 +69,30 @@ describe('StreamShow', ()=>{
     })
 })
 
-// describe('StreamEdit', ()=>{
-//     it("can display a form to edit a specific stream when you click on the edit button", ()=>{
-//         wrapper.find('.home').at(0).simulate('click',  { button: 0 });
-//         wrapper.find('.edit-stream').at(0).simulate('click',  { button: 0 });
-//         expect(wrapper.containsMatchingElement(<StreamForm/>)).toBeTruthy();   
-//     })
-//     // it('can update the form inputs and stream in the redux store/DB and display the updated stream once redirected', (done) =>{
-//     //     wrapper.find('input#title').simulate('change', {
-//     //         target: { value: 'Test' }
-//     //     })
-//     //     wrapper.find('input#description').simulate('change', {
-//     //         target: { value: 'edited' }
-//     //     })
- 
-//     //     expect(wrapper.find('input#title').props().value).toEqual('Test')
-//     //     expect(wrapper.find('input#description').props().value).toEqual('edited')
-//     //     // mocking the patched stream that would be sent back from the server
-//     //     let editedStream = {...streams['5c245be47ce56d21479e6eca']};
-//     //     editedStream.title = 'Test';
-//     //     editedStream.description = 'edited';
-//     //     // console.log(editedStream);
-        
-//     //     moxios.install();
-//     //     moxios.stubRequest('/api/streams/5c245be47ce56d21479e6eca', {
-//     //         status: 200,
-//     //         response: editedStream
-//     //     })
-//     //     wrapper.find('.form').simulate('submit');
-//     //     moxios.wait(()=> {
-//     //         wrapper.update();
-//     //         console.log(wrapper.debug());
-//     //         expect(wrapper.find('.header').at(1).text()).toEqual('Test');
-//     //         expect(wrapper.find('.description').text()).toEqual('edited');
-//     //         moxios.uninstall();
-//     //         done();
-//     //     }) 
-//     // })
-// })
+describe('StreamEdit', ()=>{
+    it("can display a form to edit a specific stream when you click on the edit button", ()=>{
+        wrapper.find('.home').at(0).simulate('click',  { button: 0 });
+        wrapper.find('.edit-stream').at(0).simulate('click',  { button: 0 });
+        expect(wrapper.containsMatchingElement(<StreamForm/>)).toBeTruthy();   
+    })
+    it('can update the form inputs and streams in redux store once form is submitted', (done) =>{
+        wrapper.find('input#title').simulate('change', {
+            target: { value: 'Edited' }
+        })
+        wrapper.find('input#description').simulate('change', {
+            target: { value: 'new stream' }
+        })
+        expect(wrapper.find('input#title').props().value).toEqual('Edited')
+        expect(wrapper.find('input#description').props().value).toEqual('new stream');
+        wrapper.find('.form').simulate('submit');
+        moxios.wait(()=> {
+            wrapper.find('.home').at(0).simulate('click',  { button: 0 });
+            expect(wrapper.find('.header').at(1).text()).toEqual('Edited');
+            expect(wrapper.find('.description').text()).toEqual('new stream');
+            done();
+        }) 
+    })
+})
 
 // describe('StreamDelete', ()=>{
 //     it("can display a modal to delete a specific stream when you click on the delete button from the homepage", ()=>{
--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -61,7 +61,7 @@ export const deleteStream = id => async dispatch =>{
 }
 
 export const editStream = (id, formValues) => async dispatch => {
-    const response = await streams.patch(`/streams/${id}`, formValues)
+    const response = await streams.patch(`/streams/${id}`, formValues);
     dispatch({
         type: EDIT_STREAM,
         payload: response.data
