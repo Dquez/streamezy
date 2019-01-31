167ce3f061b29e5227b3f61216412012c01a6f03
Dariell Vasquez <dariellv@yahoo.com>
2019-01-31 13:53:52 -0500

    added .env file for mongodb URI, updated port to 3001

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 const bodyParser = require("body-parser");
 const streamsController = require("./controllers/streamsController");
 
