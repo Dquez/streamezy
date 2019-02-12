6be4d72defffce5371d777a8b3c2c649e1f1ebde
Dariell Vasquez <dariellv@yahoo.com>
2019-02-11 22:41:26 -0500

    updated StreamEdit to use Modal component, added small styling

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -3,7 +3,9 @@ import _ from 'lodash';
 import {connect} from 'react-redux';
 import {fetchStream, editStream} from '../../actions';
 import StreamForm from './StreamForm'
-
+import Modal from '../Modal';
+import {Link} from 'react-router-dom';
+import history from '../../history';
 class StreamEdit extends Component{
     state = { streamId : this.props.match.params.id}
     componentDidMount(){
@@ -12,17 +14,29 @@ class StreamEdit extends Component{
     onSubmit = formValues =>{
         this.props.editStream(this.state.streamId, formValues);
     }
+    renderContent () {
+        return <StreamForm 
+                    initialValues={_.pick(this.props.stream, 'title', 'description')}
+                    onSubmit={this.onSubmit} 
+                />;
+    }
+    renderActions () {
+        return <Link to='/' className='ui button'>X</Link>;
+    }
     render(){
-        if(!this.props.stream){
+        const {stream} = this.props;
+        if(!stream){
             return <> Loading...</>
         }
         return (
-            <React.Fragment>
-                <h3>Edit a Stream</h3>
-                <StreamForm 
-                initialValues={_.pick(this.props.stream, 'title', 'description')}
-                onSubmit={this.onSubmit} />
-            </React.Fragment>   
+            <div className='StreamEdit'>
+                <Modal 
+                    title={`Edit ${stream ? stream.title : ' Stream'}`}
+                    content={this.renderContent()}
+                    onDismiss={()=> history.push('/')}
+                    actions={this.renderActions()}
+                />
+            </div>   
         )
     }
     
