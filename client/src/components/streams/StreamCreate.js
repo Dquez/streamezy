30d9a7c61dfbc628954359b9616d7b0d5a19e679
Dariell Vasquez <dariellv@yahoo.com>
2019-02-11 22:32:08 -0500

    updated StreamCreate to use Modal component, added small styling

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,18 +2,31 @@ import React, {Component} from 'react';
 import {createStream} from '../../actions';
 import {connect} from 'react-redux';
 import StreamForm from './StreamForm';
+import history from '../../history';
+import Modal from '../Modal';
+import {Link} from 'react-router-dom';
+import '../assets/css/StreamCreate.css';
 
 class StreamCreate extends Component{
     onSubmit = formValues => {
         this.props.createStream(formValues);
     }
-    
+    renderContent() {
+        return <StreamForm onSubmit={this.onSubmit}/>;
+    }
+    renderActions () {
+        return <Link to='/' className='ui button'>X</Link>;
+    }
     render(){
         return(
-           <React.Fragment>
-               <h3>Create a Stream</h3>
-               <StreamForm onSubmit={this.onSubmit}/>
-           </React.Fragment>
+           <div className='StreamCreate'>
+               <Modal 
+                title='Create a Stream'
+                content={this.renderContent()}
+                onDismiss={()=>history.push('/')}
+                actions={this.renderActions()}
+                />
+           </div>
         )
     }
 }
