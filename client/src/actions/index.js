0377e8e5de3db2b9bf532ce711ba563ad316918e
Dariell Vasquez <dariellv@yahoo.com>
2019-03-08 17:08:43 -0500

    backend port bug fix

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -24,7 +24,6 @@ export const signOut = () => {
 export const createStream = formValues => async (dispatch, getState) => {
     const {userId} = getState().auth;
     const response = await streams.post('/streams', {...formValues, userId});
-    // console.log(response);
     dispatch({
         type: CREATE_STREAM,
         payload: response.data
@@ -33,6 +32,7 @@ export const createStream = formValues => async (dispatch, getState) => {
 }
 export const fetchStreams = () => async dispatch =>{
     const response = await streams.get('/streams');
+    console.log(response);
     dispatch({
         type: FETCH_STREAMS,
         payload: response.data
